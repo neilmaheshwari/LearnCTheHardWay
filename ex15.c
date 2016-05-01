ff4900c8bcdeee0ee370fcafb37807e7338aa841
Neil Maheshwari <Neil@Neils-MacBook-Pro.local>
2016-04-30 17:32:34 -0700

    Rewrites ex15 looping through array backwards

--- ex15.c
+++ ex15.c
@@ -54,10 +54,47 @@ int main(int argc, char *argv[])
     printf("---\n");
 
     // fourth way with pointers in a stupid complex way
-    for(cur_name = names, cur_age = ages; (cur_age - ages) < count; cur_name++, cur_age++)
-    {
+    for(cur_name = names, cur_age = ages; (cur_age - ages) < count; cur_name++, cur_age++){
 	printf("%s lived %d years so far.\n", *cur_name, *cur_age);
     }
 
+    printf("---\n");
+    
+    // fifth way using indexing, starting at end of the array
+    for (i = 0; i < count; i++){
+	printf("[Backwards] %s has %d years alive.\n",
+	       names[count - 1 - i], ages[count - 1 - i]);
+    }
+
+    printf("---\n");
+
+    // sixth way using pointers, starting at end of the array
+    cur_age = ages;
+    cur_name = names;
+    for (i = 0; i < count; i++){
+	printf("[Backwards] %s is %d years old.\n",
+	       *(cur_name + count - i - 1),
+	       *(cur_age + count - i - 1));
+    }
+
+    printf("---\n");
+
+    // seventh way, pointers are just arrays, starting at end of
+    // the array
+    for(i =0; i < count; i++) {
+	printf("[Backwards] %s is %d years old again.\n",
+	       cur_name[count - i - 1],
+	       cur_age[count - i - 1]);
+    }
+    printf("---\n");
+
+    // eighth way, with pointers in a stupid complex way,
+    // starting at the end of the array
+    for(cur_name = names + count - 1, cur_age = ages + count - 1;
+	(cur_age - ages) >= 0; cur_name--, cur_age--) {
+	printf("[Backwards] %s lived %d so far.\n",
+	       *cur_name, *cur_age);
+    }
+    
     return 0;
 }

5998cbaff4b811ec752857856cced66abac2b1c0
Neil Maheshwari <Neil@Neils-MacBook-Pro.local>
2016-05-22 16:18:22 -0700

    Adds comments about build flags in ex22

--- ex22/ex22.sh
+++ ex22/ex22.sh
@@ -3,6 +3,13 @@
 set -e
 
 # Compile ex22.c and ex22_main.c
+# -g flag generates debug information
+# -c flag  runs preprocessor, parser, type checking, LLVM generation,
+# optimization, target-spceific code generation, produces an assembly file,
+# and assembler, generating a target ".o" object file
+# -o <file_name> flag writes output to file
+# -DNDEBUG sets the NDEBUG flag
+
 cc -Wall -g -DNDEBUG -c -o ex22.o ex22.c
 cc -Wall -g -DNDEBUG ex22_main.c ex22.o -o ex22_main
 

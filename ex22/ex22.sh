8ac392a344437b33fc3186d2a39682f673ffa175
Neil Maheshwari <Neil@Neils-MacBook-Pro.local>
2016-05-22 18:39:05 -0700

    Uses makefile for ex22

--- ex22/ex22.sh
+++ ex22/ex22.sh
@@ -10,8 +10,10 @@ set -e
 # -o <file_name> flag writes output to file
 # -DNDEBUG sets the NDEBUG flag
 
-cc -Wall -g -DNDEBUG -c -o ex22.o ex22.c
-cc -Wall -g -DNDEBUG ex22_main.c ex22.o -o ex22_main
+# cc -Wall -g -DNDEBUG -c -o ex22.o ex22.c
+# cc -Wall -g -DNDEBUG ex22_main.c ex22.o -o ex22_main
+
+make all
 
 # Run ex22
 ./ex22_main
